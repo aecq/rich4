382d4c02d4ebe5c0750bf2e5a4a79b8b6da5eec3
aecq <44742805+aecq@users.noreply.github.com>
2026-06-15 00:00:00 +0800

    add struct map node type comment

--- csrc/mkf/functional/include/core/types/map.h
+++ csrc/mkf/functional/include/core/types/map.h
@@ -15,7 +15,7 @@ struct MapNode {  // 大小: 40 字节 (0x28)
     int16_t y;                   // [0x02] Y 坐标
     char name[20];               // [0x04] 地名
     int16_t neighbors[4];        // [0x18] 邻接点数组下标
-    int16_t type;                // [0x20]
+    int16_t type;                // [0x20] 2000~3999: 普通土地 4000~5999: 设施; 6001~7999: 上市企业
     int16_t chunk;               // [0x22] Map 图像资源 chunk 下标
     int16_t special;             // [0x24] 特殊地点, 否则 0
     int16_t fork;                // [0x28] 0 环路点不含分叉点; 非 0 分叉点和非环路点
